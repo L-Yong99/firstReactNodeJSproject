2b201d4923da1f523f4f5a53ecebd8fbe9f648ba
leon boon <l.yung5982@gmail.com>
2023-01-16 15:38:04 +0800

    handle update/put request in NodeJS

--- 2.NodeJs&Expressframework/index.js
+++ 2.NodeJs&Expressframework/index.js
@@ -31,14 +31,9 @@ app.get('/api/courses/:id', (req, res) => {
 });
 
 app.post('/api/courses', (req, res) => {
-  const schema = Joi.object({
-    name: Joi.string().min(3).required()
-  });
-
-  const validation = schema.validate(req.body);
-
-  if (validation.error) {
-    res.status(400).send(validation.error.details[0].message);
+  const { error } = validateCourse(req.body); // this is equal to validation.error
+  if (error) {
+    res.status(400).send(error.details[0].message);
     return;
   }
 
@@ -51,6 +46,57 @@ app.post('/api/courses', (req, res) => {
   res.send(course);
 });
 
+
+app.put('/api/courses/:id', (req, res) => {
+  // check if id exist
+  // if invalid return error 404
+
+  //validate
+  //if invalid return 400 - bad request
+
+  //update course
+  // return updated course
+
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course) res.status(404).send('The course with the given ID was not found');
+
+  // const validation = validateCourse(req.body);
+  const { error } = validateCourse(req.body); // this is equal to validation.error
+
+  if (error) {
+    res.status(400).send(error.details[0].message);
+    return;
+  }
+
+  // if (validation.error) {
+  //   res.status(400).send(validation.error.details[0].message);
+  //   return;
+  // }
+
+  course.name = req.body.name;
+  res.send(course);
+
+
+});
+
+function validateCourse(course) {
+  const schema = Joi.object({
+    name: Joi.string().min(3).required()
+  });
+
+  return schema.validate(course);
+};
+
+
 // proper way to setup port
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening to port ${port}...`));
+
+
+
+// const schema = Joi.object({ name: Joi.string() .min(6) .required(),
+//   email: Joi.string() .min(6) .required() .email(),
+//   password: Joi.string() .min(6) .required() });
+
+//   const validation = schema.validate(req.body);
+//    res.send(validation);
